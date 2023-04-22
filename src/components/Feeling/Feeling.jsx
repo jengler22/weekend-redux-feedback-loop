1a596fd9a0661bcc796f34381024684ed79c685d
Jakob Engler <jengler2291@gmail.com>
2023-04-22 13:56:40 -0500

    created reducer for feeling.jsx and made feeling.jsx functional

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,9 +1,33 @@
-
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function Feeling () {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const feelingType = useSelector(store => store.feelingType);
+    const handleChange = (e) => {
+        const action = { type: 'SET_FEELING_TYPE', payload: event.target.value};
+        dispatch(action);
+    }
+    const nextPage = (event) => {
+        event.preventDefault();
+        if (feelingType.length > 0) {
+            history.push('/understanding');
+        } else {
+            alert('We would love to know how you feel!');
+        }
+    }
 
     return(
         <>
+
+<h3>/////</h3>
+            <form onSubmit={nextPage}>
+                <label htmlFor="feeling-type">.....</label><br />
+                <input id="feeling-type" value={feelingType} onChange={handleChange} type="text" />
+                <input type="submit" value="Next" />
+            </form>
         
         </>
     )
--- src/index.js
+++ src/index.js
@@ -2,10 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+
+const feelingType = (state = '', action) => {
+
+    if(action.type === 'SET_FEELING_TYPE') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_FORM') {
+        return '';
+    }
+      return state;
+}
+
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            feelingType,
+           
+        }
+    ),
+    applyMiddleware(logger)
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store={ storeInstance}>
         <App />
+        </Provider>
     </React.StrictMode>
 );
