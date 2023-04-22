aca56239ae0e7c2ed66ca1a09f601a840b399847
Jakob Engler <jengler2291@gmail.com>
2023-04-22 11:26:26 -0500

    imported all components to app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Support from '../Support/Support';
+import Comments from '../Comments/Comments';
+import Review from '../Review/Review';
 import './App.css';
+
+
 
 function App() {
 
