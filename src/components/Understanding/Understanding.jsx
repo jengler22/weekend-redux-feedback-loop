6e35873d6ade3e2df37cac4014a10021c816fe1d
Jakob Engler <jengler2291@gmail.com>
2023-04-23 13:27:02 -0500

    added logic code to understanding.jsx

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -2,7 +2,23 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 function Understanding () {
-    
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const understandingInfo = useSelector(store => store.understandingInfo);
+    const handleChange = (e) => {
+        const action = { type: 'SET_UNDERSTANDING', payload: event.target.value};
+        dispatch(action);
+    }
+    const nextPage = (event) => {
+        event.preventDefault();
+        if (understandingInfo.length > 0) {
+            history.push('/support');
+        } else {
+            alert('We would love to the oppourtunity to improve!');
+        }
+    }
+
 
     return(
         <>
