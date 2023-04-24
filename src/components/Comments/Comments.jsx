5344afb6e0f83516fa66669137ec7412c2c69341
Jakob Engler <jengler2291@gmail.com>
2023-04-24 14:42:11 -0500

    review.jsx fully functional

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -4,8 +4,31 @@ import { useHistory } from 'react-router-dom';
 
 function Comments() {
 
-    return(
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const addComments = useSelector(store => store.addComments);
+    const handleChange = (e) => {
+        const action = { type: 'SET_COMMENTS', payload: event.target.value};
+        dispatch(action);
+    }
+    const nextPage = (event) => {
+        event.preventDefault();
+        if (addComments.length > 0) {
+            history.push('/review');
+        } else {
+            alert('Anything else we should know?');
+        }
+    }
+
+   return(
         <>
+        <h3>Tell us more</h3>
+            <form onSubmit={nextPage}>
+                <label htmlFor="add-comments">.....</label><br />
+                <input id="add-commemts" value={addComments} onChange={handleChange} type="text" />
+                <input type="submit" value="Next" />
+            </form>
         
         </>
     )
