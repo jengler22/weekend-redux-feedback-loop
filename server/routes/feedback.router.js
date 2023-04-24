8c9229a08f38d08463955cb1e32ef4b57524207c
Jakob Engler <jengler2291@gmail.com>
2023-04-24 16:37:11 -0500

    created POST request in router.js

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -14,3 +14,16 @@ router.get('/', (req, res) => {
         res.sendStatus(500);
     })
 });
+
+// POST request
+router.post('/', (req, res) => {
+    console.log('In POST request');
+    let queryText = 'INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);';
+    let { feeling, understanding, support, comments } = req.body;
+    pool.query(queryText, [feeling, understanding, support, comments]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    })
+});
